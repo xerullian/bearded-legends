6313b783d35e1424503b74836a57785c53b79872
Loopthing <loopthing8@gmail.com>
2024-08-06 08:50:53 -0400

    Make the UTC clock slightly more accurate (it is always late by 50millis but that's as close as I can get right now).

--- src/App/WarTimer/Header/Clock.jsx
+++ src/App/WarTimer/Header/Clock.jsx
@@ -7,31 +7,26 @@ import content from '@content/Content.yaml';
 import stringToTemplate from '../../../utils/stringToTemplate';
 
 export default function Clock({ className, timeZone = 'UTC' }) {
-  const [tick, start] = useInterval({ strict: true });
+  const [tick, start] = useInterval({ strict: true, onTheMinute: true });
   const b = useContentBundle(content);
-  const [clock, setClock] = useState('');
-  const [secondsDisplay, setSecondsDisplay] = useState('');
-  const [progress, setProgress] = useState(0);
+  const [clock, setClock] = useState({});
   const CLOCK_FORMAT = b.CLOCK_FORMAT();
 
   useEffect(() => start(), []);
 
   useEffect(() => {
     const now = new Date();
-    const hour = now.getUTCHours();
-    const minute = now.getUTCMinutes();
+    const hours = now.getUTCHours();
+    const minutes = now.getUTCMinutes();
     const seconds = now.getUTCSeconds();
+    const millis = now.getUTCMilliseconds();
 
-    setProgress(hour * 60 + minute);
-    setSecondsDisplay(String(seconds).padStart(2, '0'));
-
-    setClock(
-      stringToTemplate(CLOCK_FORMAT, {
-        hour: String(hour).padStart(2, '0'),
-        minute: String(minute).padStart(2, '0'),
-        // seconds: String(seconds).padStart(2, '0'),
-      }),
-    );
+    setClock({
+      hours,
+      minutes: String(minutes).padStart(2, '0'),
+      seconds: String(seconds).padStart(2, '0'),
+      millis: String(millis).padStart(3, '0'),
+    });
   }, [tick]);
 
   // TODO Abstract formatting and reconsider seconds display / feedback
@@ -40,8 +35,8 @@ export default function Clock({ className, timeZone = 'UTC' }) {
     <div className={Arrays.pack(className, Styles.Clock).join(' ')}>
       <div className={Styles.Time}>
         <div className={Styles.TimeDisplay}>
-          {clock}
-          <small>:{secondsDisplay}</small>
+          {clock.hours}:{clock.minutes}
+          <small>.{clock.seconds}</small>
         </div>
         <div className={Styles.TimeZone}>{timeZone}</div>
       </div>
--- src/hooks/useInterval.jsx
+++ src/hooks/useInterval.jsx
@@ -1,11 +1,23 @@
 import { useState } from 'react';
 
-export default function useInterval({ delay = 1000, strict = false } = {}) {
+export default function useInterval({
+  delay = 1000,
+  strict = false,
+  onTheMinute,
+} = {}) {
   const [tid, setTid] = useState(0);
   const [tick, setTick] = useState(false);
 
   const getDelay = () => {
-    const elapsed = Date.now() - tick;
+    const now = new Date();
+    const elapsed = now.getTime() - tick;
+
+    if (onTheMinute) {
+      // This gets relatively close to firing the event when the second hand
+      // hits 0 on the clock, but on my machine, it is consistently late by
+      // roughly 50ms. This is close enough for now.
+      return delay - now.getUTCMilliseconds();
+    }
 
     if (strict) {
       return delay - elapsed;
