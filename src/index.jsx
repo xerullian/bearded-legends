fb8eafeebabca2e27b5f83aa299d261613199276
Loopthing <loopthing8@gmail.com>
2024-08-11 16:35:09 -0400

    Still trying to get github pages to work.

--- src/index.jsx
+++ src/index.jsx
@@ -1,37 +1,38 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import {
-  createBrowserRouter,
-  RouterProvider,
-  useRouteError,
-} from 'react-router-dom';
+// import {
+//   createBrowserRouter,
+//   RouterProvider,
+//   useRouteError,
+// } from 'react-router-dom';
 import './index.css';
-import App from './App/App';
+// import App from './App/App';
 import WarTimer from './App/WarTimer/WarTimer';
 
-const router = createBrowserRouter([
-  {
-    path: '/bearded-legends',
-    element: <App />,
-    errorElement: <RouterError />,
-  },
-  {
-    path: '/bearded-legends/war-timer',
-    element: <WarTimer />,
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: '/bearded-legends',
+//     element: <App />,
+//     errorElement: <RouterError />,
+//   },
+//   {
+//     path: '/bearded-legends/war-timer',
+//     element: <WarTimer />,
+//   },
+// ]);
 
 const $root = document.querySelector('#root');
 const root = createRoot($root);
 
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    {/* <RouterProvider router={router} /> */}
+    <WarTimer />
   </React.StrictMode>,
 );
 
-function RouterError() {
-  const routerError = useRouteError();
+// function RouterError() {
+//   const routerError = useRouteError();
 
-  return <h1>Error</h1>;
-}
+//   return <h1>Error</h1>;
+// }
